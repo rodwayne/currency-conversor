dfbfff4488ef18e3c81dbf8d68555372d556b55a
rodwayne <ricardo260898@gmail.com>
2024-05-02 18:05:32 -0500

    Update

--- src/Currency.java
+++ src/Currency.java
@@ -1,2 +1,2 @@
-public record Currency(String base_code, String target_code, double conversion_result) {
+public record Currency(String base_code, String target_code, double conversion_rate, double conversion_result) {
 }
